4584795b9cdc939bfb797e3c09012460909d2720
Alice P <alice.panek@gmail.com>
2013-11-25 22:33:10 -0800

    Revised locator script
    Script now makes use of lists. Will revise soon to use JSON format as
    well.

--- Layout/arm_lyt.py
+++ Layout/arm_lyt.py
@@ -1,8 +1,25 @@
 import maya.cmds as cmds
+import json
 
-# creates list to hold locator data
-locData = ['loc1', 'loc2', 'loc3', 'loc4']
+# creates empty list to hold locator info
+locator_info = []
 
-# populates list with locators, positions and names based on list data
-for loc in locData:
-	cmds.spaceLocator(n=(loc), p=(locData.index(loc), 0, 0))
+# creates list to hold locator data. Will change later to accept text input as basis of list.
+locList = ['lctr_l_arm1', 'lctr_l_arm2', 'lctr_l_wrist', 'lctr_l_armEnd']
+
+# populates empty list with locators, positions and names based on input list data
+def createLocators(locData, destList):
+    for loc in locData:
+	    cmds.spaceLocator(n=(loc), a=True)
+	    cmds.xform (t=(locData.index(loc), 0, 0))
+	    destList.append([loc, cmds.xform (q=True, t=True)])
+
+
+createLocators(locList, locator_info)
+
+# print statements
+print locator_info
+
+for locs in locator_info:
+	print locs[0]
+	print locator_info.index(locs)
